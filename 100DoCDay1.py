2b6cda7ac3e3f234cb54d09f496281556d4ac238
Tyler Kenny <32109685+tylerkennyy@users.noreply.github.com>
2023-10-26 16:54:37 -0400

    Add files via upload

---
+++ 100DoCDay1.py
@@ -0,0 +1,13 @@
+#Day 1 - Python Print Function"
+#The function is declared like this:
+#print('what to print')
+
+## We are going to be making these 3 lines into a printed statement via Terminal
+
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+
+#Pass
